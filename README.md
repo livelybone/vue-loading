1d64c0d5fceaf4dcbe222e66387c37cd9a0d74c6
livelybone <2631541504@qq.com>
2018-08-10 17:15:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# @livelybone/vue-loading
+A vue loading component
+
+## repository
+https://github.com/livelybone/vue-loading.git
+
+## Demo
+https://github.com/livelybone/vue-loading#readme
+
+## TODO
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+const fs = require('fs');
+
+function getEntries() {
+  const reg = /\.vue$/;
+  return fs.readdirSync(path.resolve(__dirname, './src/component'))
+    .filter(filename => reg.test(filename) && !fs.statSync(path.resolve(__dirname, './src/component', filename)).isDirectory())
+    .map(filename => ({ [filename.replace(reg, '')]: path.resolve(__dirname, './src/component', filename) }));
+}
+
+const config = {
+  mode: 'production',
+  entry: { index: './src/components/Index.vue' },
+  output: {
+    path: path.resolve(__dirname, './lib'),
+    filename: './[name].js',
+    library: 'VueSnackbar',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, exclude: /node_modules/, loader: 'vue-loader' },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ['vue-style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'stage-2'],
+          env: {
+            test: {
+              plugins: ['istanbul'],
+            },
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+};
+
+module.exports = config;
