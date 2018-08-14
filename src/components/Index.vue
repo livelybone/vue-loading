c5a1d4ab95e1bc5ffd28186dc7d9f3c7a66a4c15
livelybone <2631541504@qq.com>
2018-08-14 14:30:14 +0800

    update Index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,15 +1,15 @@
 <template>
   <svg :width="s" :height="s" viewBox="0 0 512 512" class="loading" style="enable-background:new 0 0 512 512;" xml:space="preserve">
 		<circle class="circle st0" :fill="color" cx="256" cy="472" r="40"/>
-		<circle class="circle st1" :fill="color" cx="256" cy="40" r="40"/>
-		<circle transform="matrix(0.809 -0.5878 0.5878 0.809 -180.048 307.3647)" class="circle st2" :fill="color" cx="383" cy="430.7" r="40"/>
-		<circle transform="matrix(0.809 -0.5878 0.5878 0.809 -23.1148 91.3647)" class="circle st3" :fill="color" cx="129" cy="81.3" r="40"/>
-		<circle transform="matrix(0.309 -0.9511 0.9511 0.309 11.8878 661.8575)" class="circle st4" :fill="color" cx="461.4" cy="322.7" r="40"/>
-		<circle transform="matrix(0.309 -0.9511 0.9511 0.309 -145.0454 178.8668)" class="circle st5" :fill="color" cx="50.6" cy="189.3" r="40"/>
-		<circle transform="matrix(0.9511 -0.309 0.309 0.9511 -35.8983 151.8518)" class="circle st6" :fill="color" cx="461.4" cy="189.3" r="40"/>
-		<circle transform="matrix(0.9511 -0.309 0.309 0.9511 -97.2594 31.4239)" class="circle st7" :fill="color" cx="50.6" cy="322.7" r="40"/>
-		<circle transform="matrix(0.5878 -0.809 0.809 0.5878 92.1279 343.3159)" class="circle st8" :fill="color" cx="383" cy="81.3" r="40"/>
-		<circle transform="matrix(0.5878 -0.809 0.809 0.5878 -295.2906 281.9548)" class="circle st9" :fill="color" cx="129" cy="430.7" r="40"/>
+		<circle transform="matrix(0.5878 -0.809 0.809 0.5878 -295.2906 281.9548)" class="circle st1" :fill="color" cx="129" cy="430.7" r="40"/>
+		<circle transform="matrix(0.9511 -0.309 0.309 0.9511 -97.2594 31.4239)" class="circle st2" :fill="color" cx="50.6" cy="322.7" r="40"/>
+		<circle transform="matrix(0.309 -0.9511 0.9511 0.309 -145.0454 178.8668)" class="circle st3" :fill="color" cx="50.6" cy="189.3" r="40"/>
+		<circle transform="matrix(0.809 -0.5878 0.5878 0.809 -23.1148 91.3647)" class="circle st4" :fill="color" cx="129" cy="81.3" r="40"/>
+		<circle class="circle st5" :fill="color" cx="256" cy="40" r="40"/>
+		<circle transform="matrix(0.5878 -0.809 0.809 0.5878 92.1279 343.3159)" class="circle st6" :fill="color" cx="383" cy="81.3" r="40"/>
+		<circle transform="matrix(0.9511 -0.309 0.309 0.9511 -35.8983 151.8518)" class="circle st7" :fill="color" cx="461.4" cy="189.3" r="40"/>
+		<circle transform="matrix(0.309 -0.9511 0.9511 0.309 11.8878 661.8575)" class="circle st8" :fill="color" cx="461.4" cy="322.7" r="40"/>
+		<circle transform="matrix(0.809 -0.5878 0.5878 0.809 -180.048 307.3647)" class="circle st9" :fill="color" cx="383" cy="430.7" r="40"/>
 	</svg>
 </template>
 
@@ -41,15 +41,15 @@ export default {
 
   @for $i from 0 through 9 {
     .st#{$i} {
-      animation-delay: #{-1+($i + 1)* .1}s;
+      animation-delay: #{-1 + ($i + 1) * .1}s;
     }
   }
 }
 
 @keyframes sk-circleFadeDelay {
   @for $i from 0 through 9 {
-    #{($i+1)*10}% {
-      opacity: #{.1 * ($i+1)};
+    #{($i + 1) * 10}% {
+      opacity: #{.1 * ($i + 1)};
     }
   }
 }
